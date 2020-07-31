0b8f3cb21a1d99f2c6a41e81a8cad1c05325a9a1
Marina Kriek <pauwmarina@gmail.com>
2020-07-31 13:59:42 +0200

    SANBI GeoJSON layer added! Note: it was necessary to simplify the 2018 vegetation map 61%.

--- wwwroot/js/map.js
+++ wwwroot/js/map.js
@@ -1,17 +1,131 @@
-import '../../node_modules/ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
+import 'ol/ol.css';
+import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import {Fill, Stroke, Style, Text} from 'ol/style';
 
-const map = new Map({
-  target: 'map',
+var style = new Style({
+  fill: new Fill({
+    color: 'rgba(255, 255, 255, 0.6)',
+  }),
+  stroke: new Stroke({
+    color: '#319FD3',
+    width: 1,
+  }),
+  text: new Text({
+
+    font: '12px Calibri,sans-serif',
+    fill: new Fill({
+      color: '#000',
+    }),
+    stroke: new Stroke({
+      color: '#fff',
+      width: 3,
+    }),
+  }),
+});
+
+
+var vectorSource = new VectorSource({
+   
+  url: '/js/SimplifiedGEOJSON.json',
+  format: new GeoJSON({"dataProjection" : 'EPSG:4326', "featureProjection": 'EPSG:4326'})
+
+
+})
+
+var biomes = new VectorLayer({
+  source: vectorSource,
+  visible: true,
+  style: function (feature) {
+    style.getText().setText(feature.get('NAME'));
+    return style;
+  },
+ 
+})
+
+
+
+
+var map = new Map({
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
-  ],
+      source: new OSM(),
+    }),
+     biomes 
+    ],
+  target: 'map',
   view: new View({
+    projection: 'EPSG:4326',
     center: [0, 0],
-    zoom: 0
-  })
+    zoom: 2,
+  }),
+});
+var highlightStyle = new Style({
+  stroke: new Stroke({
+    color: '#f00',
+    width: 1,
+  }),
+  fill: new Fill({
+    color: 'rgba(255,0,0,0.1)',
+  }),
+  text: new Text({
+    font: '12px Calibri,sans-serif',
+    fill: new Fill({
+      color: '#000',
+    }),
+    stroke: new Stroke({
+      color: '#f00',
+      width: 3,
+    }),
+  }),
+});
+
+var featureOverlay = new VectorLayer({
+  source: new VectorSource(),
+  map: map,
+  style: function (feature) {
+    highlightStyle.getText().setText(feature.get('NAME'));
+    return highlightStyle;
+  },
+});
+
+var highlight;
+var displayFeatureInfo = function (pixel) {
+  var feature = map.forEachFeatureAtPixel(pixel, function (feature) {
+    return feature;
+  });
+
+  var info = document.getElementById('info');
+  if (feature) {
+    info.innerHTML = feature.get('NAME');
+  } else {
+    info.innerHTML = '&nbsp;';
+  }
+
+  if (feature !== highlight) {
+    if (highlight) {
+      featureOverlay.getSource().removeFeature(highlight);
+    }
+    if (feature) {
+      featureOverlay.getSource().addFeature(feature);
+    }
+    highlight = feature;
+  }
+};
+
+map.on('pointermove', function (evt) {
+  if (evt.dragging) {
+    return;
+  }
+  var pixel = map.getEventPixel(evt.originalEvent);
+  displayFeatureInfo(pixel);
+});
+
+map.on('click', function (evt) {
+  displayFeatureInfo(evt.pixel);
 });
